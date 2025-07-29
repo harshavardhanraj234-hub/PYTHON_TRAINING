24caca3139ecc723ea10050373ca3fc4e126d111
harshavardhanraj234-hub <harshavardhanraj234@gmail.com>
2025-07-29 11:20:25 +0530

    HARSHA

---
+++ pavan.py
@@ -0,0 +1,99 @@
+#num = input("enter num1")
+#num2 = input("enter num2")
+#try:
+ #   if num.isnumeric() == False:
+  #      print("num is not number")
+  #  elif num2.isnumeric() == False:
+   #     print("num2 is not number")
+    #else:
+     #   res = int(num)+int(num2)
+      #  print(str(res))
+#except exception as e:
+ #   print(e)
+#------------------------------------------------
+#num=int (input("enter a number"))
+##try:
+#    if num>100:
+#        raise("number is greater")
+ #   else:
+  #      print(num)
+#except Exception as e:
+ #   print(e)
+#------------------------------------------------
+#num = int(input("enter the number"))
+#if num<0:#
+    #print("number is negative")
+#elif num==0:
+ #   print("number is zero ")
+#else:
+ #   print("number is positive")
+#----------------------------------------------------
+
+#num=[20,30,16]
+#largest=max(num)
+#print("the largest number is",largest)
+#-------------------------------------------
+##umber = -3
+#print(abs(number))
+#----------------------------------------
+
+
+# num1 = int(input("enter the num1"))
+# num2 = int(input("enter the num2"))
+# action =num1+num2
+# print(action)
+
+# num1 = int(input("enter the num1"))
+# num2 = int(input("enter the num2"))
+# action =num1-num2
+# print(action)
+
+
+# num1 = int(input("enter the num1"))
+# num2 = int(input("enter the num2"))
+# action =num1*num2
+# print(action)
+
+
+# num1 = int(input("enter the num1"))
+# num2 = int(input("enter the num2"))
+# action =num1/num2
+# char_count = {};
+# name = "hi how are you"
+# for c in name:
+#     if not c in char_count:
+#        char_count[c]=1
+#     else:
+#       char_count[c]+=1
+    
+    
+# print(char_count)
+# name = "awsedf"
+# num = 45;
+# print(f"{name}+{num} ")
+
+
+# def print_user_info(name,city):
+#     text=f"Name is {name}.and his city is {city}"
+#     print(text)
+# name = input("enter name:")
+# city = input("enter city:")
+# print_user_info(name,city)
+# def printTime(data,time):
+#     print(f"the data is{data} and the time is {time}")
+
+# printTime({23},{14:15});
+# Ask the user for their date of birth
+
+
+
+
+# def calculate_age(dob_str):
+#     # Convert string to datetime object
+#     dob = date_time.strptime(dob_str, "%Y-%m-%d")
+#     today = date_time.today()
+#     # Calculate age
+#     age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+#     return age
+    
+
